5b3db28ff742e458745f36a9afc8f194079e17d7
Jason <maroon5mlj@gmail.com>
2017-02-16 10:56:34 +0800

    Update for toy_app:register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  protect_from_forgery :except => :get_majors
   
   def hello
       render html:"hello world"
